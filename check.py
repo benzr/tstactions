9ebcd50b928f8378588e7839e1e774c37cdb6cf7
BenZr <benoit.zerr@ensta-bretagne.org>
2025-12-17 22:16:10 +0100

    check actions on push

--- check.py
+++ check.py
@@ -3,7 +3,8 @@ import random
 
 def main():
     """Main function that returns a random integer between 0 and 20"""
-    return random.randint(0, 20)
+    #return random.randint(0, 20)
+    return 19
 
 if __name__ == "__main__":
     print(main())
