d6443246852de37ab9008340b19c49ba76ea44da
Ben Blop <benblop@gmail.com>
2025-12-23 12:53:32 +0100

    try full run of eval with img3

--- docker/run-headless-py.sh
+++ docker/run-headless-py.sh
@@ -2,6 +2,6 @@
 cd /opt/CoppeliaSim_Edu_V4_10_0_rev0_Ubuntu24_04
 ./coppeliaSim.sh  -h -s 60000 -q /scenes/dartv2_final_v0_simple.ttt &
 cd /eval    
-python3 $1.py > $1_log.txt
+python3 $1.py > $1_log.txt 2> $1_errlog.txt
 
 
